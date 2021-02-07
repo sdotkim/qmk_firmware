a588586dc04419b871f0c700719f87196fa46b13
Stanislav Kim <stanislav.kim@memberize.me>
2021-02-07 14:12:18 -0500

    Updated keymap and enabled tapdance

--- keyboards/boardwalk/keymaps/memberizeme/keymap.c
+++ keyboards/boardwalk/keymaps/memberizeme/keymap.c
@@ -1,4 +1,5 @@
 #include "memberizeme.h"
+#include "tapdance.h"
 
 // Layer shorthand
 enum layer {
@@ -24,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_LBRC, KC_RBRC,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX, \
    KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      XXXXXXX, XXXXXXX,         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_RSFT, \
    KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_BSPC, TD_KCEN,         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS, \
-   KC_LEAD, XXXXXXX, KC_LALT, KC_LGUI,   LOWER,   KC_SPC,     TD_KCSP, XXXXXXX,       KC_ENT,   RAISE, KC_RALT,  KC_APP, XXXXXXX, XXXXXXX  \
+   KC_LEAD, XXXXXXX, KC_LALT, KC_LGUI,   LOWER,   KC_SPC,      KC_SPC, XXXXXXX,       KC_ENT,   RAISE, KC_RALT,  KC_APP, XXXXXXX, XXXXXXX  \
  ),
 
  [_RAISE] = LAYOUT_memberizeme(
@@ -44,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ),
 
  [_GM] = LAYOUT_memberizeme(
-    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      XXXXXXX, XXXXXXX,      _______, _______, _______, _______, _______, _______, \
-      KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,      XXXXXXX, XXXXXXX,      _______, _______, _______, _______, _______, _______, \
-      KC_G, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,      XXXXXXX, XXXXXXX,      _______, _______, _______, _______, _______, _______, \
-      KC_B, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,      XXXXXXX, XXXXXXX,      _______, _______, _______, _______, _______, _______, \
-   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT,  KC_SPC,      KC_SPC,  XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC  \
+    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      XXXXXXX, XXXXXXX,      _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______, \
+      KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,      XXXXXXX, XXXXXXX,      _______,   KC_P7,   KC_P8,   KC_P9, KC_PPLS, _______, \
+      KC_G, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,      XXXXXXX, XXXXXXX,      _______,   KC_P4,   KC_P5,   KC_P6, XXXXXXX, _______, \
+      KC_B, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,      XXXXXXX, XXXXXXX,      _______,   KC_P1,   KC_P2,   KC_P3, KC_PENT, _______, \
+   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT,  KC_SPC,      KC_SPC,  XXXXXXX,      XXXXXXX,   KC_P0, KC_PDOT, XXXXXXX, XXXXXXX, KC_BSPC  \
  ),
 
  [_META] = LAYOUT_memberizeme(
