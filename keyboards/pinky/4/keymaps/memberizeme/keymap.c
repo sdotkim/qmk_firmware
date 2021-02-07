bae6c80a083bf91c042cdbd601a1fdd1282af571
Stanislav Kim <stanislav.kim@memberize.me>
2021-02-07 13:42:13 -0500

    Updated Pinky4 tapdance and keymap.

--- keyboards/pinky/4/keymaps/memberizeme/keymap.c
+++ keyboards/pinky/4/keymaps/memberizeme/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "memberizeme.h"
+#include "tapdance.h"
 
 // Layer shorthand
 enum layer {
@@ -40,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, _______, _______, _______,    _______, _______, _______, _______ \
   ),
   [_LOWER] = LAYOUT( \
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,\
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,\
-       KC_GRV,  TD_MNS,  TD_EQL,  TD_BRC,  TD_CRB,  TD_PRN, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT,\
-      KC_TILD, TD_UNDO, TD_COPY, TD_PSTE, KC_BSPC,  KC_DEL, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DQUO,\
-                                 _______, _______, _______, _______,    _______, _______, _______, _______ \
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, XXXXXXX, _______,\
+      _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,    KC_NLCK,   KC_P7,   KC_P8,   KC_P9, KC_PAST, XXXXXXX, _______,\
+       KC_GRV,  TD_MNS,  TD_EQL, XXXXXXX, KC_BSPC,  KC_DEL, XXXXXXX,    XXXXXXX,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, XXXXXXX, KC_QUOT,\
+      KC_TILD, TD_UNDO, TD_COPY, TD_PSTE, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PENT,   KC_P1,   KC_P2,   KC_P3, KC_PMNS, XXXXXXX, KC_DQUO,\
+                                 _______, _______, _______, _______,      KC_P0, KC_PDOT, _______, _______ \
   ),
   [_META] = LAYOUT( \
-      KC_MAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(0),     TO(5), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(1),     TO(6), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(3),     TO(7), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(4),     TO(8), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLEP,\
+      KC_MAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(0),       TO(5), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   TO(0),\
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(1),       TO(6), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(3),       TO(7), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  TO(4),       TO(8), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLEP,\
                                  _______, _______, _______, _______,    _______, _______, _______, _______ \
   )
 };
